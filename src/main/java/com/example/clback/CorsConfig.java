4b2b1f229b15d07d9cd5c81f967356c7b1c2a74f
millejuice <potgusdavy@gmail.com>
2024-10-26 13:01:04 +0900

    api method 수정

--- src/main/java/com/example/clback/CorsConfig.java
+++ src/main/java/com/example/clback/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 // localhost:3000에서 오는 요청을 허용
                 .allowedOrigins("http://localhost:3000")
                 // GET, POST, PUT, DELETE 요청을 허용
-                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedMethods("GET","POST","PATCH","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
